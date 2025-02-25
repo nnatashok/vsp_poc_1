a0426eb148daca7d925d0d1b2c06332e4c8743e0
mastodon <124bit@gmail.com>
2025-02-25 07:23:47 -0800

    no msg

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-import re
+import json
+from enum import StrEnum
 from os import makedirs, remove
 from os.path import exists, join
+from typing import List
 
 import ffmpeg
-from dash.dash_table.FormatTemplate import percentage
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 import pandas as pd
 
 from twelvelabs import TwelveLabs
 from twelvelabs.models.task import Task
+from pydantic import BaseModel
+from openai import OpenAI
 
 videos_dir = 'videos'
 temp_dir = 'temp'
 df_gt = pd.read_csv('exercise videos - Ground truth.csv', index_col='UID')
 tw_client = TwelveLabs(api_key='tlk_3D5EH0Z1ME6JYR2S9C1S40XYZPFC')
 tw_index_id = '67bd08f347ebd55b1eab9e54'
+
+oai_client = OpenAI(api_key='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')
 
 select_category_prompt = '''
 Select the category and subcategory of the video:
@@ -24,31 +29,79 @@ Flexibility: Yoga, Stretching, Pilates
 Strength: Body weight, Weight workout, Calisthenics
 '''[1:]
 
-select_complexity_prompt = '''
+class WorkoutCategoryEnum(StrEnum):
+    Cardio = 'Cardio'
+    Felxibility = 'Felxibility'
+    Strength = 'Strength'
+
+class WorkoutSubCategoryEnum(StrEnum):
+    Walking = 'Walking'
+    Running = 'Running'
+    HIIT = 'HIIT'
+    Indoor_biking = 'Indoor biking'
+    Indoor_rowing = 'Indoor rowing'
+    Treadmill = 'Treadmill'
+    Elliptical = 'Elliptical'
+    Yoga = 'Yoga'
+    Stretching = 'Stretching'
+    Pilates = 'Pilates'
+    Body_weight = 'Body weight'
+    Weight_workout = 'Weight workout'
+    Calisthenics = 'Calisthenics'
+
+class WorkoutCategory(BaseModel):
+    category: WorkoutCategoryEnum
+    subcategory: WorkoutSubCategoryEnum
+
+set_complexity_prompt_general = '''
 Set the percentage for each category to describe the video:
 Beginner exercises
 Intermediate exercises
 Advanced exercises
 '''[1:]
 
-# for [Strength, Cardio]
-body_parts_usage_prompt = '''
-Set what percentage of the video each body part is used extensively:
-Arms
-Legs
-Chest
-Back
+set_complexity_prompt_yoga = '''
+You are a yogi with excellent flexibility. What percentage of the exercises in the workout in the video would be easy, medium and difficult for you?
 '''[1:]
 
+class WorkoutComplexity(BaseModel):
+    beginner_exercises_percent: int
+    intermediate_exercises_percent: int
+    advanced_exercises_percent: int
+    beginner_exercises: List[str]
+    intermediate_exercises: List[str]
+    advanced_exercises: List[str]
+
+# for [Strength, Cardio]
+set_body_parts_usage_prompt = '''
+Set what percentage of the video each body part is used extensively:
+Arms
+Chest
+Back
+Legs
+'''[1:]
+
+class BodyPartsEnum(StrEnum):
+    Arms = 'Arms'
+    Chest = 'Chest'
+    Back = 'Back'
+    Legs = 'Legs'
+
+class BodyPartsUsage(BaseModel):
+    arms_usage_percent: int
+    chest_usage_percent: int
+    back_usage_percent: int
+    legs_usage_percent: int
+
 # for [Flexibility]
-flexibility_type_prompt = '''
+select_flexibility_type_prompt = '''
 Select the category of the video:
 Range of motion
 Balance
 '''[1:]
 
 # for [Strength]
-strenth_type_prompt = '''
+select_strength_type_prompt = '''
 Select the category of the video:
 Muscle endurance
 Hypertrophy
@@ -57,7 +110,7 @@ Maximal strength
 '''[1:]
 
 # for [Cardio]
-cardio_type_prompt = '''
+select_cardio_type_prompt = '''
 Select the category of the video:
 Heart Rate Zone 1 - super easy effort
 Heart Rate Zone 2 - is a bit more complicated, as it should feel pretty easy, at least in the beginning
@@ -65,7 +118,7 @@ HIIT - High Intensity Interval Training (30s to 10m reps and rest)
 Functional Threshold (or anaerobic threshold training)
 '''[1:]
 
-required_equipment_prompt = '''.
+select_required_equipment_prompt = '''.
 Select what equipment from the list is used in the video, if any:
 Mat
 Dumbbells
@@ -76,8 +129,19 @@ Treadmill
 Elliptical
 '''[2:]
 
+chatgpt_prompt = 'Extract workout information.'
+
 class NonYoutubeVideoException(Exception):
     pass
+
+prompt_completion_separator = '\n#######################################################\n\n'
+def read_completion(filepath):
+    return open(filepath).read().split(prompt_completion_separator)[1]
+
+def write_prompt_and_completion(filepath, prompt, competition):
+    assert not exists(filepath)
+    with open(filepath, 'w') as f:
+        f.write(f'{prompt}{prompt_completion_separator}{competition}')
 
 def process_video(uid):
     video_name = df_gt.loc[uid, 'Name']
@@ -135,15 +199,72 @@ def process_video(uid):
         tw_video_id = open(tw_video_id_filepath, 'r').read()
         print(f'Twelvelabs video id found {tw_video_id}')
 
-    category_text_filepath =  join(video_dir, 'category.txt')
-    if not exists(category_text_filepath):
+    category_txt_filepath =  join(video_dir, 'category.txt')
+    if not exists(category_txt_filepath):
         print('Requesting category text')
-        category_text = tw_client.generate.text(video_id=tw_video_id, prompt=select_category_prompt).data
-        with open(category_text_filepath, 'w') as f:
-            f.write(category_text)
+        category_completion = tw_client.generate.text(video_id=tw_video_id, prompt=select_category_prompt).data
+        print('Category text received')
+        write_prompt_and_completion(category_txt_filepath, select_category_prompt, category_completion)
     else:
         print('Category text found')
-        category_text = open(category_text_filepath, 'r').read()
+        category_completion = read_completion(category_txt_filepath)
+
+    category_json_filepath = join(video_dir, 'category.json')
+    if not exists(category_json_filepath):
+        print('Requesting category json')
+        completion = oai_client.beta.chat.completions.parse(
+            model="gpt-4o-2024-08-06",
+            messages=[
+                {"role": "system", "content": chatgpt_prompt},
+                {"role": "user", "content": category_completion},
+            ],
+            response_format=WorkoutCategory,
+        )
+        if completion.choices[0].message.refusal:
+            raise Exception('ChatGPT refusal')
+        category_dict = completion.choices[0].message.parsed.model_dump(mode='json')
+        print(f'Category json received {category_dict}')
+        with open(category_json_filepath, 'w') as f:
+            # noinspection PyTypeChecker
+            json.dump(category_dict, f, indent=2)
+    else:
+        category_dict = json.load(open(category_json_filepath, 'r'))
+        print(f'Category json found {category_dict}')
+
+    complexity_txt_filepath =  join(video_dir, 'complexity.txt')
+    set_complexity_prompt = set_complexity_prompt_yoga if category_dict['subcategory'] == 'Yoga' else set_complexity_prompt_general
+    if not exists(complexity_txt_filepath):
+        print('Requesting complexity text')
+        complexity_completion = tw_client.generate.text(video_id=tw_video_id, prompt=set_complexity_prompt).data
+        print('Complexity text received')
+        write_prompt_and_completion(complexity_txt_filepath, set_complexity_prompt, complexity_completion)
+    else:
+        print('Complexity text found')
+        complexity_completion = open(complexity_txt_filepath, 'r').read()
+        
+    complexity_json_filepath = join(video_dir, 'complexity.json')
+    if not exists(complexity_json_filepath):
+        print('Requesting complexity json')
+        completion = oai_client.beta.chat.completions.parse(
+            model="gpt-4o-2024-08-06",
+            messages=[
+                {"role": "system", "content": chatgpt_prompt},
+                {"role": "user", "content": complexity_completion},
+            ],
+            response_format=WorkoutComplexity,
+        )
+        if completion.choices[0].message.refusal:
+            raise Exception('ChatGPT refusal')
+        complexity_dict = completion.choices[0].message.parsed.model_dump(mode='json')
+        print(f'Complexity json received {complexity_dict}')
+        with open(complexity_json_filepath, 'w') as f:
+            # noinspection PyTypeChecker
+            json.dump(complexity_dict, f, indent=2)
+    else:
+        complexity_dict = json.load(open(complexity_json_filepath, 'r'))
+        print(f'Complexity json found {complexity_dict}')
+
+
 
 process_video('41b8e1e9-4a8c-4a4c-a426-35c041a9d8b6')
 
